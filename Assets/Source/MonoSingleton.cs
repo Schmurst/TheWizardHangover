72e3e1d352e507e707ef6f74a63cfc0437983eb9
SamHayhurst <samhayhurst@gmail.com>
2018-08-31 21:42:52 +0100

    add monosingleton

---
+++ Assets/Source/MonoSingleton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    static T m_me;
+
+    public static T Me { get { return m_me; }}
+
+    void Awake()
+    {
+        m_me = this as T;
+    }
+}
