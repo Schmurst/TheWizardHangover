824d1ab3d73ed35d1cf3821e900e5d12e48e7832
amy-phillips <amy@islovely.co.uk>
2018-09-01 15:28:00 +0100

    add level transition script - just call FadeCanvas.GetComponent<level_transition>().LoadNextScene(3.0f, 2.0f); to skip to the next scene with fade effects

--- Assets/click_on_things.cs
+++ Assets/click_on_things.cs
@@ -1,101 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+
 
 public class click_on_things : MonoBehaviour {
     public GameObject FadeCanvas = null;
-    private Image[] Images;
-    private static click_on_things m_Instance = null;
-    private string m_LevelName = "";
-    private int m_LevelIndex = 0;
-    private int ActiveImageIndex = 0;
-    private bool m_Fading = false;
-
-    private static click_on_things Instance
-    {
-        get
-        {
-            if (m_Instance == null) {
-                m_Instance = (new GameObject("click_on_things")).AddComponent<click_on_things>();
-            }
-            return m_Instance;
-        }
-    }
-
-    public static bool Fading
-    {
-        get { return Instance.m_Fading; }
-    }
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(this);
-        DontDestroyOnLoad(FadeCanvas);
-        m_Instance = this;
-        ActiveImageIndex = 0;
-        FadeCanvas.SetActive(false);
-        Images = FadeCanvas.GetComponentsInChildren<Image>();
-    }
-
-    private void DrawQuad(Color aColor, float aAlpha)
-    {
-        // Canvas
-        FadeCanvas.SetActive(true);
-        FadeCanvas.GetComponent<CanvasGroup>().alpha = aAlpha;
-        foreach (Image image in Images) {
-            image.enabled = false;
-        }
-        Images[ActiveImageIndex].enabled = true;
-    }
-
-    private IEnumerator Fade(float aFadeOutTime, float aFadeInTime, Color aColor)
-    {
-        float t = 0.0f;
-        while (t < 1.0f) {
-            ActiveImageIndex = (int)(Images.Length * t);
-
-            yield return new WaitForEndOfFrame();
-            t = Mathf.Clamp01(t + Time.deltaTime / aFadeOutTime);
-            DrawQuad(aColor, t);
-        }
-        if (m_LevelName != "")
-            SceneManager.LoadScene(m_LevelName);
-        else
-            SceneManager.LoadScene(m_LevelIndex);
-        while (t > 0.0f) {
-            yield return new WaitForEndOfFrame();
-            t = Mathf.Clamp01(t - Time.deltaTime / aFadeInTime);
-            DrawQuad(aColor, t);
-        }
-        m_Fading = false;
-    }
-    [EditorDebugMethod]
-    private void StartFade(float aFadeOutTime, float aFadeInTime, Color aColor)
-    {
-        m_Fading = true;
-        StartCoroutine(Fade(aFadeOutTime, aFadeInTime, aColor));
-    }
-
-    public static void LoadScene(string aLevelName, float aFadeOutTime, float aFadeInTime, Color aColor)
-    {
-        if (Fading) return;
-        Instance.m_LevelName = aLevelName;
-        Instance.StartFade(aFadeOutTime, aFadeInTime, aColor);
-    }
-    public static void LoadScene(int aLevelIndex, float aFadeOutTime, float aFadeInTime, Color aColor)
-    {
-        if (Fading) return;
-        Instance.m_LevelName = "";
-        Instance.m_LevelIndex = aLevelIndex;
-        Instance.StartFade(aFadeOutTime, aFadeInTime, aColor);
-    }
-   
-
+ 
     // Use this for initialization
     void Start () {
-        FadeCanvas.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -107,7 +20,7 @@ public class click_on_things : MonoBehaviour {
 
             if (Physics.Raycast(ray, out hit, 100)) {
                 Debug.Log(hit.transform.gameObject.name);
-                LoadScene("bedroom",3.0f,2.0f,Color.red);
+                FadeCanvas.GetComponent<level_transition>().LoadNextScene(3.0f, 2.0f);
             }
         }
 
---
+++ Assets/level_transition.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class level_transition : MonoBehaviour {
+    public Image[] Images;
+    public int NextSceneIndex = 1;
+    public bool Fading = false;
+
+    public void LoadNextScene(float aFadeOutTime, float aFadeInTime)
+    {
+        if (Fading) return;
+        StartFade(aFadeOutTime, aFadeInTime);
+    }
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);
+        NextSceneIndex = 1;
+        ResetFade();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void ResetFade()
+    {
+        Fading = false;
+        gameObject.SetActive(false);
+        Images = GetComponentsInChildren<Image>();
+    }
+
+    private void DrawQuad(float aAlpha, int active_image_index)
+    {
+        // Canvas
+        GetComponent<CanvasGroup>().alpha = aAlpha;
+        foreach (Image image in Images) {
+            image.enabled = false;
+        }
+        Images[active_image_index].enabled = true;
+    }
+
+    private IEnumerator Fade(float aFadeOutTime, float aFadeInTime)
+    {
+        float t = 0.0f;
+        while (t < 1.0f) {
+            yield return new WaitForEndOfFrame();
+            t = Mathf.Clamp01(t + Time.deltaTime / aFadeOutTime);
+            int active_image_index = Mathf.Clamp((int)(Images.Length * t),0, Images.Length-1);
+            DrawQuad(t, active_image_index);
+        }
+        SceneManager.LoadScene(NextSceneIndex++);
+        while (t > 0.0f) {
+            yield return new WaitForEndOfFrame();
+            t = Mathf.Clamp01(t - Time.deltaTime / aFadeInTime);
+            int active_image_index = Mathf.Clamp((int)(Images.Length * t), 0, Images.Length - 1);
+            DrawQuad(t, active_image_index);
+        }
+        ResetFade();
+    }
+
+    private void StartFade(float aFadeOutTime, float aFadeInTime)
+    {
+        Fading = true;
+        gameObject.SetActive(true);
+        StartCoroutine(Fade(aFadeOutTime, aFadeInTime));
+    }
+}
