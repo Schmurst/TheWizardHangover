5aad5948eac08473b9753da6c251e485e1612380
amy-phillips <amy@islovely.co.uk>
2018-09-01 15:27:02 +0100

    tidied up camera scroller code

--- Assets/camera_scrolly.cs
+++ Assets/camera_scrolly.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class camera_scrolly : MonoBehaviour {
-    public float mouse_percentage;
+    public float mouse_fraction;
 
     // Use this for initialization
     void Start () {
@@ -20,14 +20,15 @@ public class camera_scrolly : MonoBehaviour {
         Camera c = Camera.main;
         Vector3 offset = new Vector3(0.0f,0.0f,0.0f);
 
-        mouse_percentage = Input.mousePosition.x / c.pixelWidth;
-        if(mouse_percentage < mouse_buffer_on_edge_of_screen) {
+        mouse_fraction = Input.mousePosition.x / c.pixelWidth;
+        mouse_fraction = Mathf.Clamp(mouse_fraction, 0.0f, 1.0f);
+        if (mouse_fraction < mouse_buffer_on_edge_of_screen) {
             // move leftwards, faster if nearer edge of screen
-            float speed = (mouse_buffer_on_edge_of_screen - mouse_percentage) / mouse_buffer_on_edge_of_screen;
+            float speed = (mouse_buffer_on_edge_of_screen - mouse_fraction) / mouse_buffer_on_edge_of_screen;
             offset = new Vector3(-speed * max_scroll_speed, 0.0f, 0.0f);
-        } else if(mouse_percentage > 1.0f - mouse_buffer_on_edge_of_screen) {
+        } else if(mouse_fraction > 1.0f - mouse_buffer_on_edge_of_screen) {
             // move rightwards, faster if nearer edge of screen
-            float speed = (mouse_percentage - 1.0f + mouse_buffer_on_edge_of_screen) / mouse_buffer_on_edge_of_screen;
+            float speed = (mouse_fraction - 1.0f + mouse_buffer_on_edge_of_screen) / mouse_buffer_on_edge_of_screen;
             offset = new Vector3(speed * max_scroll_speed, 0.0f, 0.0f);
         }
 
