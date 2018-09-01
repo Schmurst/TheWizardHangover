cc13964acb662dcbc66c39d0d880b914986d1be7
amy-phillips <amy@islovely.co.uk>
2018-09-01 14:38:42 +0100

    smoother camera scrolling  -faster at edges

--- Assets/camera_scrolly.cs
+++ Assets/camera_scrolly.cs
@@ -12,7 +12,7 @@ public class camera_scrolly : MonoBehaviour {
 
     [Range(0.0f, 0.3f)]
     public float mouse_buffer_on_edge_of_screen = 0.1f;
-    public float scroll_speed = 0.05f;
+    public float max_scroll_speed = 0.05f;
     public Vector2 scene_extremities = new Vector2(-10.0f, 10.0f);
 
     // Update is called once per frame
@@ -22,12 +22,13 @@ public class camera_scrolly : MonoBehaviour {
 
         mouse_percentage = Input.mousePosition.x / c.pixelWidth;
         if(mouse_percentage < mouse_buffer_on_edge_of_screen) {
-            // move leftwards
-            offset = new Vector3(-scroll_speed, 0.0f, 0.0f);
-
+            // move leftwards, faster if nearer edge of screen
+            float speed = (mouse_buffer_on_edge_of_screen - mouse_percentage) / mouse_buffer_on_edge_of_screen;
+            offset = new Vector3(-speed * max_scroll_speed, 0.0f, 0.0f);
         } else if(mouse_percentage > 1.0f - mouse_buffer_on_edge_of_screen) {
-            // move rightwards
-            offset = new Vector3(scroll_speed, 0.0f, 0.0f);
+            // move rightwards, faster if nearer edge of screen
+            float speed = (mouse_percentage - 1.0f + mouse_buffer_on_edge_of_screen) / mouse_buffer_on_edge_of_screen;
+            offset = new Vector3(speed * max_scroll_speed, 0.0f, 0.0f);
         }
 
         transform.position += offset;
