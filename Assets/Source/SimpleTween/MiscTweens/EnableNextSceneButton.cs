31bb4e92c54a245bc84d6e1af3e4a34a28e11b9e
SamHayhurst <samhayhurst@gmail.com>
2018-09-01 18:04:39 +0100

    add first pass on game play event tween

---
+++ Assets/Source/SimpleTween/MiscTweens/EnableNextSceneButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableNextSceneButton : MonoBehaviour, SimpleTween.ITweenEvent
+{
+    void SimpleTween.ITweenEvent.Play()
+    {
+        var levelTrans = FindObjectOfType<level_transition>();
+
+    }
+}
