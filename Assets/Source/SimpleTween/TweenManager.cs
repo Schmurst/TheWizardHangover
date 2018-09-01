8419a117783f4a013e84d1cb45d487edbb561e50
SamHayhurst <samhayhurst@gmail.com>
2018-09-01 16:18:13 +0100

    audio tweens tho

--- Assets/Source/SimpleTween/TweenManager.cs
+++ Assets/Source/SimpleTween/TweenManager.cs
@@ -13,17 +13,24 @@ namespace SimpleTween
 		Rotation,		
 		Hue,
         AnimatorPlay,
+        CustomTween,
 
 		NullOrLength
 	}
 	
+    public interface ITweenEvent
+    {
+        void Play();
+    }
+
 	public static class TweenManager
 	{
 		public static readonly Dictionary<Type, HashSet<TweenType>> UsableTweensByType = new Dictionary<Type, HashSet<TweenType>>
 		{
 			{typeof(Transform), new HashSet<TweenType> {TweenType.Position, TweenType.Scale, TweenType.Rotation}},
 			{typeof(Image), 	new HashSet<TweenType> {TweenType.Hue}},
-            {typeof(Animator),  new HashSet<TweenType> {TweenType.AnimatorPlay}}
+            {typeof(Animator),  new HashSet<TweenType> {TweenType.AnimatorPlay}},
+            {typeof(ITweenEvent),  new HashSet<TweenType> {TweenType.CustomTween}}
         };  
 	}
 }
