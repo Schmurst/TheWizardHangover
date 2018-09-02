e203cfda8c09fd191c3d9f3d338776c23b485b1f
Lawrence Shahid <lawshahid@gmail.com>
2018-09-02 10:48:15 +0100

    Added script to enable Next Scene Button in PlayMaker

---
+++ Assets/TheWizardHangover/Code/NextScenePlaymaker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScenePlaymaker : MonoBehaviour {
+
+    public void EnableNextScene()
+    {
+        var levelTrans = FindObjectOfType<level_transition>();
+        levelTrans.EnableButton();
+    }
+}
